03e8461ba60e0447bb7e238477cc241343761658
Julio Cesar de Almeida Marcelino <julio.marcelino@clear.sale>
2025-07-22 15:26:03 -0300

    chore: configura semantic-release

--- release.config.js
+++ release.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   branches: ["main"],
+  repositoryUrl: "https://github.com/jcamarcelino/observability-lib.git",
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/npm",
     "@semantic-release/git",
-    "@semantic-release/github", // Adicione este
+    "@semantic-release/github",
   ],
 };
