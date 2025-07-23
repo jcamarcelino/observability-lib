34b674c6c9b9014eacf35c7c8304d74da693954a
Júlio Marcelino <jcamarcelino@gmail.com>
2025-07-23 10:10:46 -0300

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -4,8 +4,15 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/npm",
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: true,
+        pkgRoot: ".",
+        registry: "https://npm.pkg.github.com/"
+      }
+    ],
     "@semantic-release/git",
-    "@semantic-release/github",
+    "@semantic-release/github"
   ],
 };
